09f39786934062253bd48ba11f4c5e1b34540941
Agus <aguss.luzenti>
2020-03-29 12:33:56 -0300

    Se agregan dependencias, se especifica java runtime, se agregan tests

---
+++ src/main/java/Exercise15.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+public class Exercise15 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BiFunction<BigDecimal, Integer, BigDecimal> biFunction = (bigDecimal, bigDecimal2) -> null;
+
+    }
+
+
+
+}
